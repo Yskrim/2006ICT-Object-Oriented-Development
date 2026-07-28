1b30cec7e26493697beb40101c62df301dc9c9bf
Anton Korotkov <anton.korotkov@griffithuni.edu.au>
2026-07-28 22:30:32 +1000

    Code blocks in Java

--- week 1/lecture1/src/Main.java
+++ week 1/lecture1/src/Main.java
@@ -1,67 +1,33 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    // Statements in Java
-    // 1 - Declaration statements -- declare a variable + optionally initialize it
-    int number;
-    String message = "Hello";
+// Code blocks in Java
+    // A code block is a group of zero or more statements enclosed in {braces}
+    // Code blocks are used to group statements together and define the scope of variables
 
-    // 2 - Expression statements -- preform an action like assignment or a method call
-    number = 10;
-    System.out.println(message);
-
-    // 3 - Control flow statements -- controls the order of statement execution
-
-
-    // 3a - Conditional statements -- execute blocks based on condition
-    if(number > 0){
-        System.out.println("Positive");
-    } else {
-        System.out.println("Non-positive");
-    }
-
-    switch (number) {
-        case 1:
-            System.out.println("One");
-        case 2:
-            System.out.println("Two");
-        default:
-            System.out.println("Other num");
-    }
-
-
-    // 3b - Looping statements
-
+    // 2 -- Loop block -- defines a loop body
     for(int i = 0; i < 10; i++){
-        System.out.println("i is " + i);
+        System.out.println(i);
     }
 
-    int i = 0;
-    while (i < 5){
-        System.out.println("i is " + i);
-        i++;
+
+    // 3 -- Conditional block -- defines conditional statement body
+    int a = 100;
+    if (a > 5){
+        System.out.println("Greater than 5");
+    } else {
+        System.out.println("Less or equal to 5");
     }
 
-    i = 0;
-    do {
-        System.out.println("i is " + i);
-        i++;
-    } while (i < 5);
 
-
-    // 3c -- Jump statements -- alter the flow of execution by jumping to another part of the program
-    for(int j=0; j<10; j++){
-        if (j==5){
-            break; // exits loop
-        }
-        if (j % 2 == 0){
-            continue; // skips iteration
-        }
-        System.out.println("i is " + j);
+    // 4 -- Anonymous block -- groups statements without being associated with any control flow or method.
+    {
+        int b = 10;
+        System.out.println("a is " + a);
     }
 }
-
-// 3c -- Jump statements
-public int add(int a, int b){
-    return a + b; // exits the method
+// 1 -- Method block -- defines the body of a method
+public void printMsg(){
+    System.out.println("Hello world");
+    // block is on the bottom because it's declared outside of main
 }
