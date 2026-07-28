2dceab270d66a122159cdc5b5fe2504c0e36a804
Anton Korotkov <anton.korotkov@griffithuni.edu.au>
2026-07-28 16:58:55 +1000

    arithmetics and methods

--- week 1/LearnJava/src/Main.java
+++ week 1/LearnJava/src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    String greeting = "Hello, world!";
-    String byes = "Good bye!";
-    int greetLen = greeting.length();
+public class Main{
+    public static void main(String[] args){
+        String greeting = "Hello, world!";
+        int num = 5;
+        int greetLen = greeting.length();
 
-    System.out.println(greetLen);
-    System.out.println(greeting.toLowerCase(Locale.ROOT));
-    System.out.println(greeting.toUpperCase(Locale.ROOT));
-    System.out.println(greeting.getBytes());
-    System.out.println(greeting.equals(byes));
-    System.out.println(greeting.equals(greeting));
+        int total =greetLen + num;
+        System.out.println(total);
 
+        int newTotal = add(greetLen, num);
+        System.out.println(newTotal);
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
 }
