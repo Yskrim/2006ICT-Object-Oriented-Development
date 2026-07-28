01091c95698c35177bb593ca5251cc35bce72187
Anton Korotkov <anton.korotkov@griffithuni.edu.au>
2026-07-28 21:59:40 +1000

    5-String pooling

--- week 1/lecture1/src/Main.java
+++ week 1/lecture1/src/Main.java
@@ -1,15 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    // 4-Integer pooling
-    // Java caches Integer values from -128 to 127 for performance
-    Integer a = 100;
-    Integer b = 100;
-    System.out.println(a == b); // true == both point to the same pooled object
+    // 5-String pooling
+    // Strings are immutable and are pooled by default when created using literals(letters)
+    String s1 = "Hello";
+    String s2 = "Hello";
+    System.out.println(s1 == s2); // true -- both point to pooled string
 
-    Integer x = 200;
-    Integer y = 200;
-    System.out.println(x == y); // false == different objects
+    String s3 = new String("Hello");
+    System.out.println(s3 == s1); //false -- different objects
 
-    System.out.println(x.equals(y)); // compare values of wrapper objects
+    System.out.println(s1.equals(s3)); // true -- compares values of objects
 }
