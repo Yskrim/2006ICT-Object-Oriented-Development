5215e4eb3d9728e6beba5bd0c4e89fad3018012c
Anton Korotkov <anton.korotkov@griffithuni.edu.au>
2026-07-28 16:38:51 +1000

    default methods on objects

--- week 1/LearnJava/src/Main.java
+++ week 1/LearnJava/src/Main.java
@@ -1,15 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    // primitive data types
-    int num = 5;
-    double price = 9.99;
-    char mark = 'A';
-    boolean isJavaFun = true;
-
-    // Non-primitive types
     String greeting = "Hello, world!";
-    int[] nums = {1, 2, 3, 4, 5};
+    String byes = "Good bye!";
+    int greetLen = greeting.length();
 
-    System.out.println(greeting);
+    System.out.println(greetLen);
+    System.out.println(greeting.toLowerCase(Locale.ROOT));
+    System.out.println(greeting.toUpperCase(Locale.ROOT));
+    System.out.println(greeting.getBytes());
+    System.out.println(greeting.equals(byes));
+    System.out.println(greeting.equals(greeting));
+
 }
