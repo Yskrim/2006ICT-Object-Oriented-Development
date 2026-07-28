5972dd7d34ef1deb620d1646926d60a9358bd0c4
Anton Korotkov <anton.korotkov@griffithuni.edu.au>
2026-07-28 21:54:20 +1000

    4-Integer pooling

--- week 1/lecture1/src/Main.java
+++ week 1/lecture1/src/Main.java
@@ -1,9 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    // 3-Autoboxing and Unboxing
-    // Java's automatic conversion between primitive types and their wrapper classes
-    Integer obj = 42; // Autoboxing int -> Integer
-    int num = obj; // Unboxing Integer -> int
-    System.out.println(num);
+    // 4-Integer pooling
+    // Java caches Integer values from -128 to 127 for performance
+    Integer a = 100;
+    Integer b = 100;
+    System.out.println(a == b); // true == both point to the same pooled object
+
+    Integer x = 200;
+    Integer y = 200;
+    System.out.println(x == y); // false == different objects
+
+    System.out.println(x.equals(y)); // compare values of wrapper objects
 }
