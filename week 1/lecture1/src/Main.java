56cb7fe455d99d20722cab2ab22d119de04b0b1e
Anton Korotkov <anton.korotkov@griffithuni.edu.au>
2026-07-28 21:21:52 +1000

    1-Primitive types

---
+++ week 1/lecture1/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    // Primitive data types:
+    byte b = 1; // 8 bit
+    short s = 1000; // 16 bit
+    int i = 42; //32 bit
+    long l = 123456L; // 64 bit
+    float f = 3.14f; // 32 bit
+    double d = 3.14; // 64 bit
+    char c = 'A'; // 16 bit
+    boolean bool = true; // 1 bit
+}
