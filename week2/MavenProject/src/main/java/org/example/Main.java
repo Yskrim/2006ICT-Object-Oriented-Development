bdfcba50c45c72c6bd7a51beed42883c01328c0f
Anton Korotkov <anton.korotkov@griffithuni.edu.au>
2026-07-31 20:46:19 +1000

    GUI with VBox: render VBox + elements, event handling on btn, set paddings + margins + alignment

--- week2/MavenProject/src/main/java/org/example/Main.java
+++ week2/MavenProject/src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 import javafx.application.Application; // Entry point for JavaFX apps
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene; // Container for all UI
 import javafx.scene.control.Label; // Node -- a UI component
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane; // layout pane -- organizes UI components
+import javafx.scene.layout.VBox; // layout pane -- organizes UI components
 import javafx.stage.Stage; // Main window
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,8 +18,21 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage primaryStage){
-        Label label = new Label("Hello from JavaFX + Maven!");
-        StackPane root = new StackPane(label);
+        Label label = new Label("Enter your name:");
+        TextField input = new TextField();
+        Button btn = new Button("Submit");
+
+        btn.setOnAction(e -> {
+            String name = input.getText();
+            label.setText("Hello, " + name);
+        });
+
+//        StackPane root = new StackPane(label, input, btn);
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(20));
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(label, input, btn);
+
         Scene scene = new Scene(root, 400, 200);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Maven Project");
